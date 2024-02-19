9c108bbebcb025d02d2c98b6412183a456895967
dubidubob <2076397@ewhain.net>
2024-02-20 04:13:32 +0900

    add: bosss patterns

---
+++ Assets/Scripts/Mermaid/Boss/randomAppear.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomAppear : MonoBehaviour
+{
+    /// <summary>
+    /// 일차배열 두개.
+    /// 1. 체크
+    /// 2. 위치 벡터값
+    /// 만약 해당자리에 폭탄이 없다면
+    /// 폭탄 생성
+    /// 체크
+    /// 
+    /// collider 빋으면 체크 해제 후 0.1초 뒤 제거
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
