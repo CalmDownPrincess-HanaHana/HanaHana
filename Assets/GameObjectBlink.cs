0700da5c1028f56023ef0e2df84885797eae7058
dubidubob <2076397@ewhain.net>
2024-02-29 03:59:08 +0900

    fix: snowmap_난이도 조절

---
+++ Assets/GameObjectBlink.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectBlink : MonoBehaviour
+{
+    //자식이 일정한 시간 동안 활성화 됐다, 안 됐다 하기.
+    [SerializeField] private float interval = 10f; //깜빡이는 간격
+    private List<Transform> children = new List<Transform>(); // 자식 오브젝트를 저장할 리스트
+
+    void Start()
+    {
+        // 모든 자식 오브젝트를 리스트에 추가
+        foreach (Transform child in transform)
+        {
+            children.Add(child);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        StartCoroutine(enableBlink(interval)); // sec초마다 폭탄 생성하는 코루틴 시작
+    }
+
+    private IEnumerator enableBlink(float interval) 
+    {
+        while (true)
+        {
+            Debug.Log("asdf");
+            // 생김
+            foreach (Transform child in children)
+            {
+                child.gameObject.SetActive(true); // 자식 오브젝트 활성화
+            }
+            yield return new WaitForSeconds(interval); // 0.5초 대기
+
+            //사라짐
+            foreach (Transform child in children) {
+                child.gameObject.SetActive(false); // 자식 오브젝트 활성화
+            }
+            yield return new WaitForSeconds(interval); // 0.5초 대기
+        }
+
+    }
+}
+
