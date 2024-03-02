5ebe15467eec24d1b7a1bf63d7ddac0bb24e77b1
dubidubob <sildredel@naver.com>
2024-03-02 17:45:28 +0900

    fix: 거북이 깃털 약물

--- Assets/Scripts/Mermaid/AdjustJumppower.cs
+++ Assets/Scripts/Mermaid/AdjustJumppower.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class AdjustJumppower : MonoBehaviour
 {
-    [SerializeField] private float jumpPower = 100f;
+    [SerializeField] private float jumpPower = 10f;
     private Rigidbody2D playerRigidbody;
     private Player playerScript;
     private GameObject player;
+    private bool isOnce=false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!isOnce&&collision.gameObject.CompareTag("Player"))
         {
+            isOnce=true;
             player = collision.gameObject;
             playerScript = player.GetComponent<Player>();
             if (playerScript != null)
             {
-                playerScript.jump_power = jumpPower;
+                playerScript.edit_jump = true;
+                playerScript.editted_jump_power=jumpPower;
             }
+        }
+    }
+
+    //함수가 호출되면 jump power를 원래대로 되돌리고, 해당 오브젝트를 파괴합니다.
+    public void ResetJumpPower()
+    {
+        if(player!=null&&playerScript != null)
+        {
+            playerScript.edit_jump=false;
+            // 현재 오브젝트를 파괴합니다.
+            Destroy(gameObject);
         }
     }
 }
